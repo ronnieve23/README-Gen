6de0b3a8a805d70db2c11850d49c9a01f62ca734
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-08-11 14:23:27 -0400

    Added the rest of the licenses for Function RenderLicense

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,7 +25,16 @@ function renderLicenseSection(license) {
   if (license ==='No License'){
     return `No License Was Used For This Project`
   }else if( license === 'Apache-2.0'){
-    return `APACHE PLACEHOLDER`
+    return `https://choosealicense.com/licenses/apache-2.0/`
+  }
+  else if( license === 'ISC'){
+    return `https://choosealicense.com/licenses/isc/`
+  }
+  else if( license === 'GPL-3.0'){
+    return `https://choosealicense.com/licenses/gpl-3.0/`
+  }
+  else if( license === 'MIT'){
+    return `https://choosealicense.com/licenses/mit/`
   }
 }
 
@@ -63,7 +72,7 @@ function generateMarkdown(data) {
   ${data.test}
 
   ## Questions
-  For any questions, please feel free to reach out to me at:
+  For any questions, please feel free to reach out to me at the following:
 
   Github: https://github.com/${data.github}
 
@@ -74,6 +83,7 @@ function generateMarkdown(data) {
 
   ### ${data.license}
 
+  Read About The License Here: 
   ${renderLicenseSection(data.license)}
 
 `;
